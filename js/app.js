2010638a546ab345370b0430abc172c258cb5e75
FayzullayevElmurod <elmurodfayzullayev77@gmil.com>
2021-12-22 18:25:28 +0500

    level

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector(".burger");
+const nav_link = document.querySelector(".nav_link");
+
+burger.addEventListener("click", () => {
+    nav_link.classList.toggle("active");
+})
