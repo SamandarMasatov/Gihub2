17efb8ef0a4a87d157d63d6c44ce733c171d92d4
TasnimSamandar <75119964+SamandarMasatov@users.noreply.github.com>
2021-12-27 20:50:17 +0500

    asdadas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const burger = document.querySelector(".burger");
-const nav_link = document.querySelector(".nav_link");
+const nav_link = document.querySelector(".nav_link_phone");
 
 burger.addEventListener("click", () => {
     nav_link.classList.toggle("active");
-}) 
+})  
