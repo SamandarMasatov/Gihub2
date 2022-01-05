bf5e38a438e68fd04f7cb754175d02acdb3fc122
Botir9977 <botirroziyev97@gmail.com>
2022-01-05 17:56:09 +0500

    create

---
+++ create/create.js/create.js
@@ -0,0 +1,7 @@
+     const navLink = document.querySelector('.nav_link');
+     const burger = document.querySelector('.burger');
+
+
+    burger.addEventListener('click', () =>{
+        navLink.classList.toggle('active')
+    })
